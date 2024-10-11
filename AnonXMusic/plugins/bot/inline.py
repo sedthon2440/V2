a0a0f164b6c03dcca64b443b654c1d4780849614
sedthon2440 <166177796+sedthon2440@users.noreply.github.com>
2024-10-11 15:38:02 +0300

    Update inline.py

--- AnonXMusic/plugins/bot/inline.py
+++ AnonXMusic/plugins/bot/inline.py
@@ -43,15 +43,15 @@ async def inline_query_handler(client, query):
                 ]
             )
             searched_text = f"""
-❄ <b>ᴛɪᴛʟᴇ :</b> <a href={link}>{title}</a>
+❄ <❄ <b>العنـوان :</b> <a href={link}>{title}</a>
 
-⏳ <b>ᴅᴜʀᴀᴛɪᴏɴ :</b> {duration} ᴍɪɴᴜᴛᴇs
-👀 <b>ᴠɪᴇᴡs :</b> <code>{views}</code>
-🎥 <b>ᴄʜᴀɴɴᴇʟ :</b> <a href={channellink}>{channel}</a>
-⏰ <b>ᴘᴜʙʟɪsʜᴇᴅ ᴏɴ :</b> {published}
+⏳ <b>المـدة :</b> {duration} ᴍɪɴᴜᴛᴇs
+👀 <b>المشاهـدات :</b> <code>{views}</code>
+🎥 <b>القنـاة :</b> <a href={channellink}>{channel}</a>
+⏰ <b>بواسطـة :</b> {published}
 
 
-<u><b>➻ ɪɴʟɪɴᴇ sᴇᴀʀᴄʜ ᴍᴏᴅᴇ ʙʏ {app.name}</b></u>"""
+<u><b>➻ تم البحث انلايـن بواسطـة {app.name}</b></u>"""
             answers.append(
                 InlineQueryResultPhoto(
                     photo_url=thumbnail,
