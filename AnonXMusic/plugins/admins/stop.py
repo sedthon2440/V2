d0d36fd39eafa939b658cb46d6d625c1e52a9166
sedthon2440 <166177796+sedthon2440@users.noreply.github.com>
2024-10-11 15:40:00 +0300

    Update stop.py

--- AnonXMusic/plugins/admins/stop.py
+++ AnonXMusic/plugins/admins/stop.py
@@ -10,14 +10,15 @@ from config import BANNED_USERS
 
 
 @app.on_message(
-    filters.command(["end", "stop", "cend", "cstop"]) & filters.group & ~BANNED_USERS
+    command(["/stop", "اسكت", "انهاء", "ايقاف"]) & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def stop_music(cli, message: Message, _, chat_id):
-    if not len(message.command) == 1:
+    if len(message.command) > 1:
         return
     await Anony.stop_stream(chat_id)
     await set_loop(chat_id, 0)
+    user_mention = message.from_user.mention if message.from_user else "المشـرف"
     await message.reply_text(
-        _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
+        _["admin_5"].format(user_mention), reply_markup=close_markup(_)
     )
